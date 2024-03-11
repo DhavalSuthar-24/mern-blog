59eaacb91564398ef94ceb68880a7f1298c1457b
DhavalSuthar-24 <dhavalll63@gmail.com>
2024-03-11 13:13:42 +0530

    headerrrr

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+// import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import Signup from './pages/Signup';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import PrivateRoute from './components/PrivateRoute';
+// import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+// import CreatePost from './pages/CreatePost';
+// import UpdatePost from './pages/UpdatePost';
+// import PostPage from './pages/PostPage';
+// import ScrollToTop from './components/ScrollToTop';
+import Search from './pages/Search';
+import Header from './Components/Header';
 const App = () => {
   return (
-    <div className='text-6xl'>App</div>
+    <BrowserRouter >
+   <Header/>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/search' element={<Search />} />
+        {/* <Route element={<PrivateRoute />}> */}
+          <Route path='/dashboard' element={<Dashboard />} />
+        {/* </Route> */}
+        {/* <Route element={<OnlyAdminPrivateRoute />}> */}
+          {/* <Route path='/create-post' element={<CreatePost />} /> */}
+          {/* <Route path='/update-post/:postId' element={<UpdatePost />} /> */}
+        {/* </Route> */}
+
+        <Route path='/projects' element={<Projects />} />
+        {/* <Route path='/post/:postSlug' element={<PostPage />} /> */}
+
+   
+   </Routes>
+    
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,56 @@
+import { Button, Navbar, TextInput } from 'flowbite-react'
+import React from 'react'
+import { Link,useLocation } from 'react-router-dom'
+import { AiOutlineSearch } from 'react-icons/ai';
+import {FaMoon} from 'react-icons/fa'
+const Header = () => {
+  const path = useLocation().pathname
+  return (
+    <Navbar className='border-b-2'>
+<Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+    <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Dk's</span>Blog
+</Link>
+<form> 
+  <TextInput className='hidden lg:inline'
+  type='text' placeholder='Search.........'
+  rightIcon={AiOutlineSearch}>
+
+  </TextInput>
+</form>
+<Button className="w-12 h-10 lg:hidden" color="grey" pill><AiOutlineSearch/></Button>
+<div className='flex gap-2 md:order-2'>
+<Button className='w-12 h-10 hidden sm:inline' color='grey' pill>
+  <FaMoon/>
+  </Button>
+  <Link to="/signin">
+    <Button color="grey" 
+   gradientDuoTone='purpleToBlue'>
+Sign In
+    </Button>
+    
+  </Link>
+  <Navbar.Toggle/>
+
+</div>
+<Navbar.Collapse>
+  <Navbar.Link active={path==="/"} as={'div'}>
+    <Link to="/">Home</Link>
+
+  </Navbar.Link>
+  <Navbar.Link active={path==="/about"} as={'div'}>
+  
+    <Link to="/about">About</Link>
+    
+  </Navbar.Link>
+  <Navbar.Link active={path==="/projects"} as={'Div'}>
+    
+  
+    <Link to="/projects">Projects</Link>
+    
+  </Navbar.Link>
+</Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default Header
