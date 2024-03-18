6150b533b3197d0f3cf73ed93e72bcba0d9a1b8f
DhavalSuthar-24 <dhavalll63@gmail.com>
2024-03-18 14:21:20 +0530

    fc

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
+import CallToAction from "../Components/CallToAction"
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="min-h-screen max-w-2xl mx-auto items-center flex justify-center flex-col gap-6 p-3">
+      <h1 className=" text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+      Feel free to explore the blog and learn along with me!
+      </p>
+      <CallToAction/>
+      </div>
   )
 }
 
